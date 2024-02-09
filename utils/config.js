81ac0eea6ab095c1aca0102c9c8e094e2941ff9d
blamine19 <amine.blh@gmail.com>
2024-02-09 14:53:36 +0000

    commit 5

--- utils/config.js
+++ utils/config.js
@@ -3,7 +3,7 @@ var domain;
 
 
 if (process.env.NODE_ENV === 'production') {
-    domain = 'https://nextjs13-crud-fmdy3psca-blamine19s-projects.vercel.app/api/'
+    domain = 'https://nextjs13-crud-ten.vercel.app/api/'
 }
 else {
     domain = 'http://localhost:3000/api/'
