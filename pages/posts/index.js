522726783ad24060b031854891e1dd87092facaf
blamine19 <amine.blh@gmail.com>
2024-02-09 14:47:57 +0000

    commit 3

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,96 +1,32 @@
-import PostItem from '@/components/PostItem'
-import domain from '@/utils/config'
-import axios from 'axios'
-import { useRouter } from 'next/router'
-import React, { useState } from 'react'
+import PostItem from "@/components/PostItem"
+import domain from "@/utils/config"
+import axios from "axios"
+  
 
 
 export default function Index({ postData }) {
 
-  console.log("=====> Componenet : index")
+  //console.log(postData)
 
-  /*console.log("------------------------------------------- DEBUT ---------------------------------------")
-  console.log(postData)
-  console.log("-------------------------------------------- FIN   --------------------------------------")*/
+  const posts = postData.map( (post)=>{
+    return <PostItem post={post}/> 
+  }
 
-  const [posts, setPosts] = useState(postData)
-
+  )
 
   return (
     <>
-      <div>index Post</div>
-      <div>
-
-        {posts.map((item, index) => (
-
-          <PostItem key={index} post={item} />
-        ))}
-
-      </div>
-
+    {posts}
     </>
-
   )
 }
 
-export async function getStaticProps() {
 
-  console.log("getStaticProps")
-
-  
-  const axios = require('axios');
-  const axios2 = require('axios');
-
-  
-  let config = {
-    method: 'get',
-    maxBodyLength: Infinity,
-    url: `${domain}/posts`,
-    headers: {
-      'Content-Type': 'application/json'
-    }  
-  };
-
-  async function makeRequest() {
-    try {
-      const response = await axios.request(config)
-      
-      //console.log(JSON.stringify(response.data))
-
-      return (response.data)
-    }
-    catch (error) {
-      console.log(error);
-    }
-  }
-
-  const dd = await makeRequest()
-
-  console.log(dd)
-
-
-  //const response2 = await axios2.get(`${domain}/posts`)
-
-  //console.log("--------------------------")
-
-  //console.log(response2.data)
-
+export async function getServerSideProps() {
+  const response = await axios.get(`${domain}/posts`)
   return {
     props: {
-      postData: dd
-    },
+      postData: response.data
+    }
   }
 }
-
-/*export async function getServerSideProps(context) {
-
-  const response = await axios.get(`${domain}/posts}`)
-
-  console.log("getServerSideProps")
-
-  return {
-    props: {
-      post: response.data
-    }
-  }
-}*/
--- utils/config.js
+++ utils/config.js
@@ -3,7 +3,7 @@ var domain;
 
 
 if (process.env.NODE_ENV === 'production') {
-
+    domain = 'http://localhost:3000/api/'
 }
 else {
     domain = 'http://localhost:3000/api/'
